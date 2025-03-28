dd65a483404b43c3cd48c56afcf7b71be6fd4f44
pallaviprakash-25 <pallaviprakash769@gmail.com>
2025-03-29 02:15:17 +0530

    Added userId field for user details

--- src/main/java/com/project/forms/controller/UserController.java
+++ src/main/java/com/project/forms/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.project.forms.controller;
 
-import com.project.forms.dao.response.UserInfo;
+import com.project.forms.dao.response.UserDetails;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -15,19 +15,21 @@ import static com.project.forms.utils.Constants.*;
 public class UserController {
 
     @GetMapping(USER)
-    public ResponseEntity<UserInfo> getUserInfo(@AuthenticationPrincipal final OAuth2User user) {
-        String name, email, pictureUrl;
+    public ResponseEntity<UserDetails> getUserInfo(@AuthenticationPrincipal final OAuth2User user) {
+        String userId, name, email, pictureUrl;
         if (getUserId(user).equals(MOCK_USER)) {
+            userId = MOCK_USER;
             name = MOCK_USER;
             email = MOCK_USER;
             pictureUrl = "#";
         } else {
+            userId = user.getAttributes().get(SUB).toString();
             name = user.getAttributes().get(NAME).toString();
             email = user.getAttributes().get(EMAIL).toString();
             pictureUrl = user.getAttributes().get(PICTURE).toString();
         }
 
-        UserInfo userInfo = new UserInfo(name, email, pictureUrl);
-        return ResponseEntity.ok(userInfo);
+        UserDetails userDetails = new UserDetails(userId, name, email, pictureUrl);
+        return ResponseEntity.ok(userDetails);
     }
 }
--- src/main/java/com/project/forms/dao/response/UserInfo.java
+++ src/main/java/com/project/forms/dao/response/UserInfo.java
@@ -1,12 +1,8 @@
 package com.project.forms.dao.response;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserInfo {
     private String sub;
     private String name;
@@ -14,14 +10,6 @@ public class UserInfo {
     private String family_name;
     private String picture;
     private String email;
-    @JsonIgnore
-    @JsonProperty
     private boolean email_verified;
     private String locale;
-
-    public UserInfo(final String name, final String email, final String picture) {
-        this.name=name;
-        this.email=email;
-        this.picture=picture;
-    }
 }
